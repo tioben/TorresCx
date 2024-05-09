f6fc4dcdc327ce0a9ab56cef5b837ce28dc76bdd
Gustavo Martins <tioben@outlook.com.br>
2024-05-09 13:30:40 -0300

    09/05/2024

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//função para fazer efeito de scroll na página atual
 function scrollToSection(className) {
     var section = document.querySelector(className);
     if (section) {
@@ -5,6 +6,7 @@ function scrollToSection(className) {
     }
 }
 
+// função para navegar até a tela e scrollar em seguida [NÃO ESTÁ FUNCIONANDO]
 function navegateAndScroll(locationPage, className) {
     window.location.href = locationPage;
     setTimeout(
@@ -16,3 +18,17 @@ function navegateAndScroll(locationPage, className) {
         }
     ,10000);
 }
+
+//Lógica para funcionamento do menu expansivo
+const elemento = document.getElementById('ulMenu');
+const checkbox = document.getElementById('meuCheckbox');
+
+let positionValue = '-160px';
+let boxShadowValue = '0px 0px 0px rgba(81, 81, 81, 0.5)';
+
+checkbox.addEventListener('change', function(){
+    positionValue = this.checked ? '90px' : '-161px';
+    boxShadowValue = this.checked ? '0px 0px 10px rgba(81, 81, 81, 0.5)' : '0px 0px 0px rgba(81, 81, 81, 0.5)'
+    elemento.style.top = positionValue;
+    elemento.style.boxShadow = boxShadowValue;
+});
