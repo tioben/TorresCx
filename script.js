9dc2ac688b1c5e226adfa38938aa7f86b715c4c0
Gustavo Martins <tioben@outlook.com.br>
2024-05-10 11:11:06 -0300

    10/05/2024

--- script.js
+++ script.js
@@ -23,12 +23,43 @@ function navegateAndScroll(locationPage, className) {
 const elemento = document.getElementById('ulMenu');
 const checkbox = document.getElementById('meuCheckbox');
 
+const spanHamburguer1 = document.getElementById('spanHamburguer1');
+const spanHamburguer2 = document.getElementById('spanHamburguer2');
+const spanHamburguer3 = document.getElementById('spanHamburguer3');
+
+// expansão do menu
 let positionValue = '-160px';
 let boxShadowValue = '0px 0px 0px rgba(81, 81, 81, 0.5)';
 
+// hamburguer
+let leftSpan1 = '-8px';
+let opacitySpan2 = '100';
+let leftSpan3 = '4px';
+let rotateSpan1 = 'rotate(0deg)';
+let rotateSpan3 = 'rotate(0deg)';
+let colorHamburguer = '#515151';
+
 checkbox.addEventListener('change', function(){
     positionValue = this.checked ? '90px' : '-161px';
-    boxShadowValue = this.checked ? '0px 0px 10px rgba(81, 81, 81, 0.5)' : '0px 0px 0px rgba(81, 81, 81, 0.5)'
     elemento.style.top = positionValue;
+    boxShadowValue = this.checked ? '0px 0px 10px rgba(81, 81, 81, 0.5)' : '0px 0px 0px rgba(81, 81, 81, 0.5)'
     elemento.style.boxShadow = boxShadowValue;
+
+    leftSpan1 = this.checked ? '-3px' : '-8px';
+    spanHamburguer1.style.left = leftSpan1;
+    leftSpan3 = this.checked ? '-0px' : '4px';
+    spanHamburguer3.style.left = leftSpan3;
+    opacitySpan2 = this.checked ? '0' : '100';
+    spanHamburguer2.style.opacity = opacitySpan2;
+
+    rotateSpan1 = this.checked ? 'rotate(-45deg)' : 'rotate(0deg)';
+    spanHamburguer1.style.transform = rotateSpan1;
+    rotateSpan3 = this.checked ? 'rotate(45deg)' : 'rotate(0deg)';
+    spanHamburguer3.style.transform = rotateSpan3;
+
+    colorHamburguer = this.checked ? '#ff0000' : '#515151';
+    spanHamburguer1.style.color = colorHamburguer;
+    spanHamburguer3.style.color = colorHamburguer;
+
+
 });
