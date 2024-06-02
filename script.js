d3599f50c6b853f72f0f6973c174dcec5f4a6cb5
Gustavo Martins <tioben@outlook.com.br>
2024-06-02 14:13:39 -0300

    02062024

--- script.js
+++ script.js
@@ -51,68 +51,68 @@ checkbox.addEventListener('change', function () {
   spanHamburguer3.style.color = colorHamburguer;
 });
 
-class FormSubmit {
-  constructor(settings) {
-    this.settings = settings;
-    this.form = document.querySelector(settings.form);
-    this.formButton = document.querySelector(settings.button);
-    if (this.form) {
-      this.url = this.form.getAttribute('action');
-    }
-    this.sendForm = this.sendForm.bind(this);
-  }
+// class FormSubmit {
+//   constructor(settings) {
+//     this.settings = settings;
+//     this.form = document.querySelector(settings.form);
+//     this.formButton = document.querySelector(settings.button);
+//     if (this.form) {
+//       this.url = this.form.getAttribute('action');
+//     }
+//     this.sendForm = this.sendForm.bind(this);
+//   }
 
-  displaySuccess() {
-    this.form.innerHTML = this.settings.success;
-  }
+//   displaySuccess() {
+//     this.form.innerHTML = this.settings.success;
+//   }
 
-  displayError() {
-    this.form.innerHTML = this.settings.error;
-  }
+//   displayError() {
+//     this.form.innerHTML = this.settings.error;
+//   }
 
-  getFormObject() {
-    const formObject = {};
-    const fields = this.form.querySelectorAll('[name]');
-    fields.forEach((field) => {
-      formObject[field.getAttribute('name')] = field.value;
-    });
-    formObject;
-    return;
-  }
+//   getFormObject() {
+//     const formObject = {};
+//     const fields = this.form.querySelectorAll('[name]');
+//     fields.forEach((field) => {
+//       formObject[field.getAttribute('name')] = field.value;
+//     });
+//     formObject;
+//     return;
+//   }
 
-  onSubmission(event) {
-    event.preventDefault();
-    event.target.disable = true;
-    event.target.innerText = 'Enviando...';
-  }
+//   onSubmission(event) {
+//     event.preventDefault();
+//     event.target.disable = true;
+//     event.target.innerText = 'Enviando...';
+//   }
 
-  async sendForm(event) {
-    try {
-      this.onSubmission(event);
-      await fetch(this.url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-        },
-        body: JSON.stringify(this.getFormObject()),
-      });
-      this.displaySuccess();
-    } catch (error) {
-      this.displayError();
-      throw new Error(error);
-    }
-  }
+//   async sendForm(event) {
+//     try {
+//       this.onSubmission(event);
+//       await fetch(this.url, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           Accept: 'application/json',
+//         },
+//         body: JSON.stringify(this.getFormObject()),
+//       });
+//       this.displaySuccess();
+//     } catch (error) {
+//       this.displayError();
+//       throw new Error(error);
+//     }
+//   }
 
-  init() {
-    if (this.form) this.formButton.addEventListener('click', this.sendForm);
-    return this;
-  }
-}
+//   init() {
+//     if (this.form) this.formButton.addEventListener('click', this.sendForm);
+//     return this;
+//   }
+// }
 
-const formSubmit = new FormSubmit({
-  form: '[data-form]',
-  button: '[data-button]',
-  success: '<h1>Mensagem enviada!</h1>',
-  error: "<h1 class='error'>Não foi possível enviar sua mensagem!</h1>",
-});
+// const formSubmit = new FormSubmit({
+//   form: '[data-form]',
+//   button: '[data-button]',
+//   success: '<h1>Mensagem enviada!</h1>',
+//   error: "<h1 class='error'>Não foi possível enviar sua mensagem!</h1>",
+// });
